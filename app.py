23aa46efc9df2ae8935c0a6639d1eec3f9006197
kwameAto <raymondghan@gmail.com>
2020-03-04 16:14:57 +0000

    my second commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# print('Enter the numeber of days')
+# numberOfDays = int(input())
+
+# numberOfYears = int(numberOfDays / 365)
+
+# numberOfWeeks = int(numberOfDays % 365/7)
+
+# remainingNumberOfDays = int(numberOfDays % 365 % 7)
+
+# print(f"Years = {numberOfYears}, weeks = {numberOfWeeks}, days = {remainingNumberOfDays} ")
+
+floatToInt = int(3.6)
+stringToInt = int('2')
+
+print(f"float to int  {floatToInt} and string to float {stringToInt} ")
